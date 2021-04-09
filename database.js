8804fc11125d45671426673088cf709187a9570f
Daniel Long <1099335751qq@gmail.com>
2021-04-09 02:03:52 -0600

    tried using mysql reconnect code from online

--- database.js
+++ database.js
@@ -1,14 +1,46 @@
 require('dotenv').config();
 const   mysql           = require('mysql');
 
+var db_config = {
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
+  };
+  
+  let connection;
+  
+  function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+  }
+
+  handleDisconnect();
+
 // const connection = mysql.createConnection({
-//     host     : process.env.DB_HOST,
-//     user     : process.env.DB_USERNAME,
-//     password : process.env.DB_PASSWORD,
-//     database : process.env.DB_NAME,
+    // host     : process.env.DB_HOST,
+    // user     : process.env.DB_USERNAME,
+    // password : process.env.DB_PASSWORD,
+    // database : process.env.DB_NAME,
 // 	autoReconnect:true
 // });
 
-const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+// const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 
 module.exports = connection;
