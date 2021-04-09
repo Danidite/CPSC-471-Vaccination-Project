b78176010d96bbd76ca13b7c22ed3e111e572da7
Daniel Long <1099335751qq@gmail.com>
2021-04-09 13:45:30 -0600

    remake new clinic page

--- routes/admin.js
+++ routes/admin.js
@@ -7,4 +7,16 @@ router.get('/', middleware.isAdmin, (req, res) => {
     res.render("admin/index");
 });
 
+router.get('/scores', middleware.isAdmin, (req, res) => {
+    connection.query("SELECT u.FName, u.MName, u.LName, p.Age, h.HealthNumber FROM USER u JOIN PATIENT p ON u.ID = p.ID JOIN HEALTH_PROFILE h ON p.ID = h.ID", function (error, result, fields) {
+        if (error) {
+            console.log("Get User score list: " + error.message);
+            req.flash("error", error.message);            
+            return res.redirect('/admin');
+        }
+        console.log(result);
+        res.render("admin/score", {users: result});
+    });
+});
+
 module.exports = router;
