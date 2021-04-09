bce7ec11be0a89302da305b725b79bd55f30c80e
Daniel Long <1099335751qq@gmail.com>
2021-04-09 01:35:57 -0600

    add auto reconnect, atempt 1

--- database.js
+++ database.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
     host     : process.env.DB_HOST,
     user     : process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+    database : process.env.DB_NAME,
+	autoReconnect:true
 });
 
 module.exports = connection;
